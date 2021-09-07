25d7966c59e5f5491e3422f8faf972296193a9ca
gfst <sotrylaspy@yandex.ru>
2021-09-07 11:45:32 +0300

    Hello world

---
+++ src/com/company/Welcome.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        String greeting = "Welcome to Core Java";
+        for (int i = 1; i <= greeting.length(); i++)
+            System.out.print("=");
+        System.out.println();
+        System.out.println(greeting);
+        for (int i = 1; i <= greeting.length(); i++)
+            System.out.print("=");
+    }
+}
